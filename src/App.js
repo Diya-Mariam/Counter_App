af67e58a13862258b087bea0e4ef473de29c72ec
Diya M <diyamariambiju25@gmail.com>
2021-08-20 15:26:52 +0530

    initial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
+
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  let [num, setNum]= useState(0);
+
+  function increment(){
+    setNum(num+1);
+  }
+
+  function decrement(){
+    setNum(num-1);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div id="showcase">
+        <p>{num}</p>
+        <div className="btn_collection">
+          <button onClick={increment}>ADD</button>
+          <button onClick={decrement}>SUB</button>
+        </div>
+      </div>    
     </div>
   );
 }
 
+          
+
 export default App;
+
